0612d73cee651c8ef46f260a97c85dffc3ae93f2
Jackie Luc <jackieluc17@gmail.com>
2018-11-07 23:06:19 -0700

    Add jquery

--- src/pages/identity/identity.js
+++ src/pages/identity/identity.js
@@ -1,14 +1,8 @@
-require('normalize.css/normalize.css');
-require('../../styles/index.scss');
+import 'normalize.css/normalize.css';
+import './styles/index.scss';
 
-document.addEventListener("DOMContentLoaded", () => {
 
-    // const pluginsTriggerElement = document.getElementById('plugins-trigger');
-    // const pluginsElement = document.getElementById('plugins');
-
-    // const pluginsVisibleClass = "splash-overview-plugins__list--visible";
-
-    // pluginsTriggerElement.onclick = () => {
-    //     pluginsElement.classList.toggle(pluginsVisibleClass);
-    // }
+$('document').ready(() => {
+    
+    $('h1').text('Identity Page');
 });

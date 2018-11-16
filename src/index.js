14105f92bac45e0154d06e756061ce04b6e63501
q-omar <q.omar@icloud.com>
2018-11-16 09:40:16 -0700

    cms theme though may be applying it to incorrect part of site

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import 'normalize.css/normalize.css';
 import './styles/index.scss';
 
 
+//not sure if this is the right place for this
+import CMS from 'netlify-cms'
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate('my-template', MyTemplate)
+
+
 $('document').ready(() => {
     
 });

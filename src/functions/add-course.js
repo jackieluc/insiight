79decf1397d8254a3f561c30c4d1eb641af2d8ac
Jackie Luc <jackieluc17@gmail.com>
2018-11-17 13:52:01 -0700

    #2 #3 - Add course (student and prof)

--- src/functions/add-course.js
+++ src/functions/add-course.js
@@ -1,7 +1,20 @@
 exports.handler = function(event, context, callback) {
   console.log('received request');
+
+  const courseJoinCode = Math.floor((Math.random() * 9999) + 1);
+
+  const payload = JSON.parse(event.body);
+  let returnMessage = '';
+
+  if (payload[0].courseCode) {
+    returnMessage = `Added course code: ${payload[0].courseCode}`;
+  }
+  else {
+    returnMessage = `Added course ${payload[0].courseName}, your course join code is ${courseJoinCode}`;
+  }
+
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: returnMessage
   });
 }
--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,15 +1,47 @@
 import 'normalize.css/normalize.css';
 import '../../styles/index.scss';
 
+const addStudentForm = () => {
+  $('#add-course-form').append(`
+    <div class="course-code input-block">
+      <label for="course-code">Join code</label>
+      <input id="course-code" name="courseCode" type="text" />
+    </div>
+  `);
+};
+
+const removeStudentForm = () => {
+  $('.course-code').remove();
+};
+
+const addProfessorForm = () => {
+  $('#add-course-form').append(`
+    <div class="course-name input-block">
+      <label for="course-name">Course name</label>
+      <input id="course-name" name="courseName" type="text" />
+    </div>
+  `);
+};
+
+const removeProfessorForm = () => {
+  $('.course-name').remove();
+};
 
 $('document').ready(() => {
 
+  // Takes the information from the form and submits it to add-course Netlify function
   $('.add-course-button').on('click', () => {
+    
+    const courseInfo = $('#add-course-form').find('input').map((index, inputField) => {
+      let courseObject = {};
+      courseObject[inputField.name] = inputField.value;
+
+      return courseObject;
+    });
+
     fetch('/.netlify/functions/add-course', {
       method: "POST",
-      body: JSON.stringify({
-        text: 'i want to add a course'
-      })
+      body: JSON.stringify(courseInfo)
     })
     .then(response => {
       if (!response.ok) {
@@ -18,5 +50,19 @@ $('document').ready(() => {
       response.text().then(console.log);
     })
     .catch(err => console.error(err));
+  });
+
+  // Toggle student and professor forms
+  $('select.role').on('change', () => {
+    let val = $('select.role').val();
+
+    if (val === 'professor') {
+      removeStudentForm();
+      addProfessorForm();
+    } 
+    else {
+      removeProfessorForm();
+      addStudentForm();
+    }
   });
 });

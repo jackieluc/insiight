94418cd71f251172b91f725b83aca2c7629b8f4d
Jackie Luc <jackieluc17@gmail.com>
2018-11-29 21:31:05 -0700

    Survey in dashboard (WIP)

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -133,3 +133,76 @@ $('document').ready(function() {
     .catch(err => console.error(err));
   });
 });
+
+
+//Survey
+//Survey.Survey.cssType = "bootstrap";
+var defaultThemeColors = Survey
+    .StylesManager
+    .ThemeColors["default"];
+defaultThemeColors["$main-color"] = "#3ED2CC";
+defaultThemeColors["$main-hover-color"] = "#83DFDB";
+
+Survey
+    .StylesManager
+    .applyTheme();
+
+var surveyJSON = {
+  questionTitleTemplate: "{no}. {title}",
+  pages: [{ 
+    name: "page1",
+    elements: [
+      {
+        type: "radiogroup",
+        name: "How are you today?",
+        colCount: 5,
+        isRequired: true,
+        choices:["Very Good","Good","OK","Bad", "Very Bad"]
+      },
+      {
+        type: "radiogroup",
+        name: "How was Class?",
+        colCount: 5,
+        isRequired: true,
+        choices: ["V.Useless","Useless","Neither","Helpful","V.Helpful"]
+      },
+      {
+        type:"radiogroup",
+        name:"question3",
+        colCount: 5,
+        isRequired:true,
+        choices:["item1","item2","item3","item4","item5"]
+      },
+      {
+        type:"radiogroup",
+        name:"question4",
+        colCount: 5,
+        isRequired: true,
+        choices:["item1","item2","item3","item4","item5"]
+      },
+      {
+        type:"radiogroup",
+        name:"question5",
+        colCount: 5,
+        isRequired:true,
+        choices:["item1","item2","item3","item4","item5"]
+      }
+    ]
+  }],
+  completeText:"Submit survey"
+}
+
+var survey = new Survey.Model(surveyJSON);
+survey.surveyPostId = '72167288-14c7-4f0e-af17-6c4955db4e9a';
+survey.surveyShowDataSaving = true;
+
+function sendDataToServer(survey) {
+    //send Ajax request to your web server.
+    alert("The results are:" + JSON.stringify(survey.data));
+    
+}
+
+$("#surveyContainer").Survey({
+    model: survey,
+    onComplete: sendDataToServer
+});
